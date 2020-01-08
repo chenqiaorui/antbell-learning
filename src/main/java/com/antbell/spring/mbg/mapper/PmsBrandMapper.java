94f57b8532630243dd797ff01e2d4b82d3555260
rich <809155736@qq.com>
2020-01-09 03:43:21 +0800

    added swagger-ui

---
+++ src/main/java/com/antbell/spring/mbg/mapper/PmsBrandMapper.java
@@ -0,0 +1,38 @@
+package com.antbell.spring.mbg.mapper;
+
+import com.antbell.spring.mbg.model.PmsBrand;
+import com.antbell.spring.mbg.model.PmsBrandExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+public interface PmsBrandMapper {
+    long countByExample(PmsBrandExample example);
+
+    int deleteByExample(PmsBrandExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(PmsBrand record);
+
+    int insertSelective(PmsBrand record);
+
+    List<PmsBrand> selectByExampleWithBLOBs(PmsBrandExample example);
+
+    List<PmsBrand> selectByExample(PmsBrandExample example);
+
+    PmsBrand selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") PmsBrand record, @Param("example") PmsBrandExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") PmsBrand record, @Param("example") PmsBrandExample example);
+
+    int updateByExample(@Param("record") PmsBrand record, @Param("example") PmsBrandExample example);
+
+    int updateByPrimaryKeySelective(PmsBrand record);
+
+    int updateByPrimaryKeyWithBLOBs(PmsBrand record);
+
+    int updateByPrimaryKey(PmsBrand record);
+}
