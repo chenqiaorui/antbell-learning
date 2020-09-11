8ebe9265967f3850adf53ba8ba95803d59db54f5
ricky <cqr@dpcn.ltd>
2020-09-11 16:49:39 +0800

    do something

--- src/main/java/com/antbell/spring/config/MyBatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.antbell.spring.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.antbell.spring.mbg.mapper")
-public class MyBatisConfig {
-}
